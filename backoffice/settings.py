8b03e8d0c01877dc51b7abbcf3ed0a9955c84fdf
tsyork <tsyork@gmail.com>
2018-05-18 15:50:38 -0700

    add pg database and shiptrack app

--- backoffice/settings.py
+++ backoffice/settings.py
@@ -77,13 +77,34 @@ WSGI_APPLICATION = 'backoffice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# DATABASES={
+#     'default':{
+#         'ENGINE':'django.db.backends.postgresql_psycopg2',
+#         'NAME':'backoffice',#nameofdatabaseonpostgresserver
+#         'USER':'cbadmin',
+#         'PASSWORD':'Fak40Jq8JTJ!0kd6',
+#         'HOST':'crunchbase-data.cffs0buuu6jg.us-west-2.rds.amazonaws.com',
+#         'PORT':'5432',
+#     }
+# }
+
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'backoffice',#nameofdatabaseonpostgresserver
+        'USER':'timyork',
+        'PASSWORD':'Magg13m0',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
---
+++ shiptrack/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShiptrackConfig(AppConfig):
+    name = 'shiptrack'
